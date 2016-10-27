a59522cb5a073ab7c0cfb58e5d75cf2e0b3da0b9
Frank Hunleth <fhunleth@troodon-software.com>
2016-10-27 11:57:10 -0400

    DONT PUSH: erlang-p1-stun: bump to version 1.0.7
    Signed-off-by: Frank Hunleth <fhunleth@troodon-software.com>

--- package/erlang-p1-stun/erlang-p1-stun.mk
+++ package/erlang-p1-stun/erlang-p1-stun.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-ERLANG_P1_STUN_VERSION = 0.9.0
+ERLANG_P1_STUN_VERSION = 1.0.7
 ERLANG_P1_STUN_SITE = $(call github,processone,stun,$(ERLANG_P1_STUN_VERSION))
 ERLANG_P1_STUN_LICENSE = GPLv2+
 ERLANG_P1_STUN_LICENSE_FILES = COPYING
-ERLANG_P1_STUN_DEPENDENCIES = erlang-p1-tls erlang-p1-utils
+ERLANG_P1_STUN_DEPENDENCIES = erlang-p1-fast-tls erlang-p1-utils
 ERLANG_P1_STUN_INSTALL_STAGING = YES
 
 $(eval $(rebar-package))
